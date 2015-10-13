5aa807a29e4ff5e4dbb822efac930392afface98
Jimmy McDonough <mcdonoje@uc.edu>
2015-10-12 22:32:23 -0400

    Initial Commit

---
+++ MyJeepTrader.Data/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJeepTrader.Data
+{
+    public class Service
+    {
+
+        private dboMyJeepTraderEntities _context;
+
+        public Service()
+        {
+            _context = new dboMyJeepTraderEntities();
+        }
+
+
+        /// <summary>
+        /// Gets all the posts.
+        /// </summary>
+        /// <returns>List of posts.</returns>
+        public IEnumerable<tPost> GetAllPosts()
+        {
+            return (from p in _context.tPosts select p).ToList();
+        } 
+    }
+}
