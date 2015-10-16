726701995ecf0e1d24a2df8392cd1fbf9984df8a
Brent <bhumphrey08@gmail.com>
2015-10-15 22:25:27 -0400

    Added PayPalService, PayPalController, and PayPal views Index and Checkout. Installed Braintree through nuget.

---
+++ MyJeepTrader.Data/PayPalService.cs
@@ -0,0 +1,25 @@
+﻿using Braintree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJeepTrader.Data
+{
+    public class PayPalService
+    {
+        public BraintreeGateway PayPalGateway()
+        {
+            var gateway = new BraintreeGateway
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = "hpjd63fycvh7j38d",
+                PublicKey = "59yht5x4w7rn4pj7",
+                PrivateKey = "6d39af34433ba58f94be9153a77eb427"
+            };
+
+            return gateway;
+        }
+    }
+}
---
+++ MyJeepTrader.Web/Controllers/PayPalController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyJeepTrader.Data;
+using Braintree;
+
+namespace MyJeepTrader.Web.Controllers
+{
+    public class PayPalController : Controller
+    {
+        // GET: PayPal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePurchase(FormCollection collection)
+        {
+            var PayPalService = new PayPalService();
+            var gateway = PayPalService.PayPalGateway();
+
+            string nonce = collection["payment_method_nonce"];
+            // Use payment method nonce here
+
+            var request = new TransactionRequest
+            {
+                Amount = 5.99M,
+                CreditCard = new TransactionCreditCardRequest
+                {
+                    Number = collection["number"],
+                    CVV = collection["cvv"],
+                    ExpirationMonth = collection["month"],
+                    ExpirationYear = collection["year"]
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.IsSuccess())
+            {
+                Transaction transaction = result.Target;
+                ViewData["TransactionId"] = transaction.Id;
+            }
+            else
+            {
+                ViewData["Message"] = result.Message;
+            }
+
+            return View();
+        }
+
+        /*Not sure how to pass this to view*/
+        public class ClientTokenHandler : IHttpHandler
+        {
+            public void ProcessRequest(HttpContext context)
+            {
+                var PayPalService = new PayPalService();
+                var gateway = PayPalService.PayPalGateway();
+                var clientToken = gateway.ClientToken.generate();//need to pass this to js. 
+                context.Response.Write(clientToken);
+            }
+
+            public bool IsReusable
+            {
+                get { return true; }
+            }
+        }
+    }
+}
