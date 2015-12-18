692beca64a1148751213ba615976df8cde601000
Brent Humphrey <bhumphrey08@gmail.com>
2015-12-17 22:11:01 -0500

    Updated the chat hub to allow private chats.
    Als updated the Chat.cshtml to test how the chat works.. it needs some javascript/jQuery love, which I cannot provide but will continue to try to do so. Added to models as well for the chat.

---
+++ MyJeepTrader.Web/Models/Chat/MessageDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyJeepTrader.Web.Models.Chat
+{
+    public class MessageDetail
+    {
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
