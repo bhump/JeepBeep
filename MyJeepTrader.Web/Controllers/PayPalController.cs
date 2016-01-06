7fea207905672a6855e456bbd23899409f338eee
Brent Humphrey <bhumphrey08@gmail.com>
2016-01-05 21:15:05 -0500

    Updated the view to make a purchase.
    Updated the PayPalController for creating subscriptions. Receiving an Invalid Nonce.. not sure why?

--- MyJeepTrader.Web/Controllers/PayPalController.cs
+++ MyJeepTrader.Web/Controllers/PayPalController.cs
@@ -6,11 +6,37 @@ using System.Web.Mvc;
 using MyJeepTrader.Data;
 using Braintree;
 using MyJeepTrader.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace MyJeepTrader.Web.Controllers
 {
     public class PayPalController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public PayPalController()
+        {
+
+        }
+
+        public PayPalController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         // GET: PayPal
         public ActionResult Index()
         {
@@ -19,7 +45,8 @@ namespace MyJeepTrader.Web.Controllers
             var PayPalService = new PayPalService();
             var gateway = PayPalService.PayPalGateway();
             var clientToken = gateway.ClientToken.generate();
-            model.ClientToken = gateway.ClientToken.generate();
+            model.ClientToken = clientToken;
+            TempData["ClientToken"] = clientToken;
             return View(model);
         }
 
@@ -31,35 +58,82 @@ namespace MyJeepTrader.Web.Controllers
         [HttpPost]
         public ActionResult Checkout(FormCollection collection)
         {
+            Service service = new Service();
+            PayPalIndexViewModel model = new PayPalIndexViewModel();
             var PayPalService = new PayPalService();
-            var gateway = PayPalService.PayPalGateway();
 
-            string nonce = collection["fake-valid-nonce"];
+            var gateway = PayPalService.PayPalGateway();
+            var user = UserManager.FindByName(User.Identity.Name);
+            var userInfo = service.GetProfileInfo(user.Id);
+            var monthly = collection["Monthly"];
+            var annual = collection["Annual"];
+            model.ClientToken = TempData["ClientToken"].ToString();
+
             // Use payment method nonce here
 
-            var request = new TransactionRequest
-            {
-                Amount = 5.99M,
-                PaymentMethodNonce = "fake-valid-nonce"
-                //CreditCard = new TransactionCreditCardRequest
-                //{
-                //    Number = collection["number"],
-                //    CVV = collection["cvv"],
-                //    ExpirationMonth = collection["month"],
-                //    ExpirationYear = collection["year"]
-                //}
-            };
+            //commented out for testing.
+            //var transRequest = new TransactionRequest
+            //{
+            //    Amount = 5.99M,
+            //    PaymentMethodNonce = "fake-valid-nonce"
+            //    //CreditCard = new TransactionCreditCardRequest
+            //    //{
+            //    //    Number = collection["number"],
+            //    //    CVV = collection["cvv"],
+            //    //    ExpirationMonth = collection["month"],
+            //    //    ExpirationYear = collection["year"]
+            //    //}
+            //};
+            //Result<Transaction> transResult = gateway.Transaction.Sale(transRequest);
 
-            Result<Transaction> result = gateway.Transaction.Sale(request);
-
-            if (result.IsSuccess())
+            if(monthly == "on")
             {
-                Transaction transaction = result.Target;
-                ViewData["TransactionId"] = transaction.Id;
+                var subRequest = new SubscriptionRequest
+                {
+                    //PaymentMethodToken = model.ClientToken,
+                    PaymentMethodNonce = "fake-valid-visa-nonce",
+                    PlanId = "rphr",
+                    Descriptor = new DescriptorRequest
+                    {
+                        Name = userInfo.FirstName + " " + userInfo.LastName
+                    }
+                };
+                Result<Subscription> subResult = gateway.Subscription.Create(subRequest);
+
+                if (subResult.IsSuccess())
+                {
+                    Subscription transaction = subResult.Target;
+                    ViewData["TransactionId"] = transaction.Id;
+                }
+                else
+                {
+                    ViewData["Message"] = subResult.Message;
+                }
             }
-            else
+            else if (annual == "on")
             {
-                ViewData["Message"] = result.Message;
+                var subRequest = new SubscriptionRequest
+                {
+                    //PaymentMethodToken = model.ClientToken,
+                    PaymentMethodNonce = "fake-valid-visa-nonce",
+                    PlanId = "7rjw",
+                    Descriptor = new DescriptorRequest
+                    {
+                        Name = userInfo.FirstName + " " + userInfo.LastName
+                    }
+                };
+
+                Result<Subscription> subResult = gateway.Subscription.Create(subRequest);
+
+                if (subResult.IsSuccess())
+                {
+                    Subscription transaction = subResult.Target;
+                    ViewData["TransactionId"] = transaction.Id;
+                }
+                else
+                {
+                    ViewData["Message"] = subResult.Message;
+                }
             }
 
             return View();
@@ -72,7 +146,6 @@ namespace MyJeepTrader.Web.Controllers
                 var PayPalService = new PayPalService();
                 var gateway = PayPalService.PayPalGateway();
                 var clientToken = gateway.ClientToken.generate();
-                //context.Response.Write(clientToken);
             }
 
             public bool IsReusable
